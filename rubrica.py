39c043a302a498167b9a657520f9ccd7086372f0
Valerio Tognozzi <vtognozzi@gmail.com>
2021-01-21 18:15:10 +0100

    aggiungo peewee funzionante ed agganciato al sistema rubrica.py funziona aggiugni e modifica salva e carica

--- rubrica.py
+++ rubrica.py
@@ -1,17 +1,29 @@
 #TecnoGeppetto
 # esercizio construire una rubrica semplice con i dizionari
 # Aggiunto la funzione di salvataggio con Shelve (Pickle)
+# ed iniziato a progettae l'accesso ai file con PeeWee.
+
+# elemento test di rubrica =   [{'nome': 'Paolo', 'cognome': 'White', 'telefono': "['772321', '888888']", 'indirizzo': 'via versi Roma'}]
+# #
 
 import yaml
 import shelve
 import peewee
 import datetime
+import os
 global rubrica
 rubrica = []
+
+# ########################################################  PeeWee  ################
+# Per utilizzare lo stesso motore rubrica.py con PeeWee non si deve usare la funzione salva come con tutte le altre librerie
+# perche' in questo caso i dati vengono trattenuti nel DB e non soostituiti tutte le volte come negli altri casi.
+# Quindi per salvare nuovi item in rubrica va utilizzato la funzione apposita (da creare)  ma non la funzione salva 
+# che duplicherebbe tutti i vecchi dati#
 
 # Creo un modello per gli elementi della rubrica
 db = peewee.SqliteDatabase('rubrica.db')
 class Rubrica(peewee.Model):
+    id = peewee.PrimaryKeyField()
     nome = peewee.CharField()
     cognome = peewee.CharField()
     indirizzo = peewee.CharField()
@@ -22,22 +34,70 @@ class Rubrica(peewee.Model):
         db_table = 'rubrica'
 
 # Qui creo l'accesso ai dati della rubrica con la libreria PEEWEE (che si basa su Pickle)
-def peewee_salva(rubrica): #
-    # Agganciato il DB, create le tabelle  e scrive in DB ok 
-    Rubrica.create_table(Rubrica)
-    elemento1 = Rubrica.create(nome='Andrea', cognome='Verdi', indirizzo='via le mani dal naso Roma', telefono=['772321','888888'])
-    elemento1.save()
+# l'effetto e' quello di un DB
+# LA FUNZIONE SALVA()  Va utilizzata solamente per creare il file di DB. per gli aggiornamenti 
+# e le modifiche vanno usate le funzioni apposite. (da creare)
+
+def peewee_salva(): # rubrica
+    # se il file 'rubrica.db' non esiste, lo creo e ci metto le tabelle che mi servono
+    if not os.path.exists("rubrica.db") :
+        # Agganciato il DB, create le tabelle  e scrive in DB ok 
+        Rubrica.create_table(Rubrica)
+    else:
+        pass
+    # Queste cose sotto servono per popolare il file rubrica.db per fare i test
+    #elemento1 = Rubrica.create(nome='Paolo', cognome='White', indirizzo='via versi Roma', telefono=['772321','888888'])
+    #print(type(elemento1))
+    #elemento1.save()
+    #######################################
+    """
+    for el in rubrica:
+        nome = el["nome"]
+        cognome = el["cognome"]
+        indirizzo = el["indirizzo"]
+        telefono = el["telefono"]
+        Rubrica.create(nome=nome, cognome=cognome, indirizzo=indirizzo, telefono=telefono)
+    """
  
 def peewee_carica():
     # lettura del DB  e carica delle variabili OK
-    tutta = Rubrica.select()
-    print(tutta)
-    for rubrica in Rubrica.select():
-        print(rubrica.nome)
-        print(rubrica.creato)
-        print(rubrica.id)
-        print(rubrica.telefono)
+    #tuttalarubrica = Rubrica.select()       # carico la SELECT dal DB
+    for elemento in Rubrica.select():       # itero la SELECT del DB e ricavo un singolo elemento
+        nome = elemento.nome
+        cognome = elemento.cognome
+        indirizzo = elemento.indirizzo
+        telefono = elemento.telefono
+        creato = elemento.creato
+        print(elemento.id)
+        rubrica.append({"nome" : nome, "cognome" : cognome, "telefono" : telefono, "indirizzo" : indirizzo})
+    print(rubrica)  # riga da cancellare
+    return rubrica 
 
+    
+def peewee_aggiungi(nome, cognome, telefono, indirizzo):
+    """ La funzione aggiunge un nuovo record al file DB  """
+    nuovo_record = Rubrica()
+    nuovo_record.nome = nome
+    nuovo_record.cognome = cognome 
+    nuovo_record.indirizzo = indirizzo
+    nuovo_record.telefono =  telefono
+    nuovo_record.save()
+    return
+
+def peewee_modifica(elemento, nome, cognome, telefono, indirizzo):
+    """ La funizone modifica il record alla posizione elemento nel DB ## 
+    NB. elemento parte da 1 (quindi non togliere 1 come negli altri casi) """
+    modifica_record = Rubrica()
+    modifica_record.id = int(elemento)
+    modifica_record.nome = nome
+    modifica_record.cognome = cognome 
+    modifica_record.indirizzo = indirizzo
+    modifica_record.telefono =  telefono
+    modifica_record.save()
+    return
+
+
+# ########################################################    Shelve    #################
 # queste due funzioni servono per testare la libreria Shelve 
 # per fare il salvataggio di file in formato binario Shelve usa Pickle per 
 # salvare su file.
@@ -55,6 +115,28 @@ def carica_shelve():
         f.close()
     # se non trova la lista rubrica dentro il file  .dat va in crash ( accade se il file non esiste o e' vuoto.) da sistemare
     return rubrica
+
+# ########################################################  Metodo TESTO con Yaml  #################
+# Yaml passa tutta la lista dentro il file di testo trasformandola in una lunga stringa 
+# ed utilizzando una specie di Markup Language.
+def salva(rubrica):
+    with open("rubrica.txt", "w") as my_rub:
+        #my_rub = open("rubrica.txt", "w")
+        my_rub.write(yaml.dump(rubrica, default_flow_style=False))
+    return 
+def carica():
+    try:
+        f = open("rubrica.txt", "r")
+    except FileNotFoundError:
+        f = open("rubrica.txt", "w")
+        rubrica = []
+        f.write(yaml.dump(rubrica, default_flow_style=False))
+        f.close()
+        f= open("rubrica.txt", "r")
+    rubrica = yaml.load(f, Loader=yaml.FullLoader)
+    f.close()
+    return rubrica
+
 
 # Qui invece costruisco la rubrica sensa librerie, solo con funzioni di base di python
 def aggiungi(nome, cognome, telefono, indirizzo):
@@ -84,22 +166,8 @@ def svuota(noparam=False):
         salva(rubrica)
         return 
 
-def salva(rubrica):
-    with open("rubrica.txt", "w") as my_rub:
-        #my_rub = open("rubrica.txt", "w")
-        my_rub.write(yaml.dump(rubrica, default_flow_style=False))
-    return 
-def carica():
-    try:
-        f = open("rubrica.txt", "r")
-    except FileNotFoundError:
-        f = open("rubrica.txt", "w")
-        rubrica = []
-        f.write(yaml.dump(rubrica, default_flow_style=False))
-        f.close()
-        f= open("rubrica.txt", "r")
-    rubrica = yaml.load(f, Loader=yaml.FullLoader)
-    f.close()
-    return rubrica
 #peewee_salva()
+#peewee_salva([{'nome': 'Maria', 'cognome': 'Rossi', 'telefono': "['772321', '888888']", 'indirizzo': 'via versi Roma'}])
+#peewee_aggiungi(nome='Federico', cognome='Rovazzi', indirizzo='via di culo  Roma', telefono=['33452321','9987888888'])
+peewee_modifica(elemento = 1, nome= "Asdrubale", cognome = "Aniscalchi", indirizzo = " via le mani dal naso", telefono = "['123213']" )
 peewee_carica()
